3b9cdfca6d2bd1700378f1cb637a2b4d20185e39
Andrei <andreiflausino@live.com>
2024-07-04 22:44:37 -0300

    shop styling almost done.

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -2,6 +2,7 @@ import { Link, useNavigate } from "react-router-dom";
 import PropTypes from "prop-types";
 import useCartItems from "../hooks/useCartItems";
 import { useState } from "react";
+import styles from "./Shop.module.css";
 // import { useEffect, useState } from "react";
 
 function Shop({ products, loading, error }) {
@@ -52,36 +53,42 @@ function Shop({ products, loading, error }) {
   }
 
   return (
-    <div>
-      <h1>The shop should appear here!</h1>
-      {loading ? (
-        <div>Loading...</div>
-      ) : error ? (
-        <p>An error happened while trying to load the page: {error}</p>
-      ) : (
-        products.map((product) => {
-          return (
-            <div key={product.title} className="product-card">
-              <h1>{product.title}</h1>
-              <img src={product.image} alt={product.title} />
-              <p>{product.description}</p>
-              <p>{product.price}</p>
-              <form>
-                <label htmlFor="quantity">Quantity</label>
-                <input
-                  type="number"
-                  id="quantity"
-                  min={0}
-                  onChange={(e) => handleQuantityChange(product.title, parseInt(e.target.value))}
-                />
-              </form>
-              <button onClick={() => handleBuy(product)}>Buy</button>
-              <button onClick={() => handleAddCart(product)}>Add to Cart</button>
-            </div>
-          );
-        })
-      )}
+    <div className={styles.shop}>
+      <h1>Shop</h1>
       {!loading && <Link to="cart">Shopping Cart ({cartItems.length})</Link>}
+      <div className={styles.products}>
+        {loading ? (
+          <div>Loading...</div>
+        ) : error ? (
+          <p>An error happened while trying to load the page: {error}</p>
+        ) : (
+          products.map((product) => {
+            return (
+              <div key={product.title} className={styles.productCard}>
+                <img src={product.image} alt={product.title} />
+                <div className={styles.content}>
+                  <h2>{product.title}</h2>
+                  <p>{product.description}</p>
+                </div>
+                <form>
+                  <label htmlFor="quantity">Quantity: </label>
+                  <input
+                    type="number"
+                    id="quantity"
+                    min={0}
+                    onChange={(e) => handleQuantityChange(product.title, parseInt(e.target.value))}
+                  />
+                </form>
+                <div className={styles.buy}>
+                  <p>${product.price}</p>
+                  <button onClick={() => handleBuy(product)}>Buy</button>
+                  <button onClick={() => handleAddCart(product)}>Add to Cart</button>
+                </div>
+              </div>
+            );
+          })
+        )}
+      </div>
     </div>
   );
 }
